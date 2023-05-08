3f7f995bc2a4ceaf4cf243184b0b46ca2151af46
Rana-usman-ul-haq <98952692+Rana-usman-ul-haq@users.noreply.github.com>
2023-05-09 00:35:23 +0500

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,8 +1,3 @@
-/**
- *Submitted for verification at Etherscan.io on 2023-04-14
-*/
-
-// Sources flattened with hardhat v2.7.0 https://hardhat.org
 
 // File @openzeppelin/contracts/utils/Context.sol@v4.4.0
 
